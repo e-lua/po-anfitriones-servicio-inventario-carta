03473d52b44fb29fb0a5b89190641547d44a3038
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-14 17:35:29 -0500

    Completamos los datos de la carta parte 6

--- repositories/carta/pg_update_schedule_list.go
+++ repositories/carta/pg_update_schedule_list.go
@@ -2,8 +2,7 @@ package repositories
 
 import (
 	"context"
-	"strconv"
-	"strings"
+	"time"
 
 	models "github.com/Aphofisis/po-anfitrion-servicio-inventario-carta/models"
 )
@@ -13,16 +12,23 @@ func Pg_Update_ScheduleRange_List(pg_schedule []models.Pg_ScheduleRange_External
 	db_external := models.Conectar_Pg_DB_External()
 
 	//Lista de actualizacion de rangos horarios
-	idschedulerange_pg, idcarta_pg, idbusiness_pg, startime_pg, endtime_pg, max_orders := []int{}, []int{}, []int{}, []string{}, []string{}, []int{}
+	idschedulerange_pg, idcarta_pg, idbusiness_pg, startime_pg, endtime_pg, max_orders := []int{}, []int{}, []int{}, []time.Time{}, []time.Time{}, []int{}
 
 	for _, sch := range pg_schedule {
 
 		for i := 0; i < sch.NumberOfFractions; i++ {
 
-			arr := strings.Split(sch.StartTime, ":")
+			tiempo_inicial, _ := time.Parse("15:04:00", sch.StartTime)
 
-			var minutos_string string
+			//Añadimos el tiempo
+			tiempo_fin := tiempo_inicial.Add(time.Minute * time.Duration(sch.MinutePerFraction))
 
+			/*var minutos_string string
+
+			tiempo,_:=time.Parse("15:04",sch.StartTime)
+
+			t:=tiempo.Add(time.Minute*30)
+			t.Clock()
 			//Minutos y Horas
 			minutos, _ := strconv.Atoi(arr[1][:2])
 			horas, _ := strconv.Atoi(arr[0])
@@ -47,20 +53,23 @@ func Pg_Update_ScheduleRange_List(pg_schedule []models.Pg_ScheduleRange_External
 				index = 1
 			}
 			hora_fin_toinsert := hora_fin[:index] + ":" + hora_fin[index:]
-			sch.StartTime = hora_fin_toinsert
+			sch.StartTime = hora_fin_toinsert*/
 			//Fin de bucle para obtener la hora fin
 
 			//Insertamos los datos en el modelo
 			idschedulerange_pg = append(idschedulerange_pg, sch.IDSchedule)
 			idcarta_pg = append(idcarta_pg, idcarta)
 			idbusiness_pg = append(idbusiness_pg, idbusiness)
-			startime_pg = append(startime_pg, sch.StartTime)
-			endtime_pg = append(endtime_pg, hora_fin_toinsert)
+			startime_pg = append(startime_pg, tiempo_inicial)
+			endtime_pg = append(endtime_pg, tiempo_fin)
 			max_orders = append(max_orders, sch.MaxOrders)
+
+			tiempo_inicial = tiempo_fin
+
 		}
 	}
 
-	q := `INSERT INTO ListScheduleRange(idcarta,idschedulemain,idbusiness,starttime,endtime,maxorders) (select * from unnest($1::int[],$2::int[],$3::int[],$4::varchar(6)[],$5::varchar(6)[],$6::int[]))`
+	q := `INSERT INTO ListScheduleRange(idcarta,idschedulemain,idbusiness,starttime,endtime,maxorders) (select * from unnest($1::int[],$2::int[],$3::int[],$4::time[],$5::time[],$6::int[]))`
 	if _, err_update := db_external.Exec(context.Background(), q, idcarta_pg, idschedulerange_pg, idbusiness_pg, startime_pg, endtime_pg, max_orders); err_update != nil {
 		return err_update
 	}

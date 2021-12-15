76f139025cd6ac4a360d13b7ad2ab53bff37b1b5
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-15 11:23:02 -0500

    Copiar de una carta a otra parte 6 - retorno de infom de carta correcta

--- repositories/carta/pg_find_ini_data.go
+++ repositories/carta/pg_find_ini_data.go
@@ -11,7 +11,7 @@ func Pg_Find_IniData(date string, idbusiness int) (models.Pg_Carta_External, err
 	var carta_ini_data models.Pg_Carta_External
 
 	db := models.Conectar_Pg_DB_External()
-	q := "SELECT c.idcarta,c.date,c.availableorders,c.visible,COUNT(e.idelement),COUNT(s.idschedulerange) FROM carta c LEFT JOIN Element e on c.idcarta=e.idcarta LEFT JOIN schedulerange s on c.idcarta=s.idcarta WHERE c.date=$1 AND c.idbusiness=$2 GROUP BY c.idcarta, c.date,c.availableorders,c.visible"
+	q := "SELECT idcarta,date,availableorders,visible,(SELECT COUNT(*) FROM Element e JOIN Carta c on e.idcarta=c.idcarta WHERE c.date=$1 AND c.idbusiness=$2),(SELECT COUNT(*) FROM ScheduleRange sr JOIN Carta c on sr.idcarta=c.idcarta WHERE c.date=$1 AND c.idbusiness=$2) FROM Carta WHERE date=$1 AND idbusiness=$2"
 	error_shown := db.QueryRow(context.Background(), q, date, idbusiness).Scan(&carta_ini_data.IDCarta, &carta_ini_data.Date, &carta_ini_data.AvailableForOrders, &carta_ini_data.Visible, &carta_ini_data.Elements, &carta_ini_data.ScheduleRanges)
 
 	if error_shown != nil {

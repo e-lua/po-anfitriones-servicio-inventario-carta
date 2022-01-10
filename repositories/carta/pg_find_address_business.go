475725ecbac632c8b2821b0fa1b3ceefc74ccf95
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-01-10 02:12:25 -0500

    Direccion con elemento

--- repositories/carta/pg_find_address_business.go
+++ repositories/carta/pg_find_address_business.go
@@ -12,7 +12,7 @@ func Pg_Find_Address(idbusiness int) (float64, float64, error) {
 	var longitude float64
 
 	db := models.Conectar_Pg_DB()
-	q := "SELECT latitude,longitude FROM idbusiness=$1"
+	q := "SELECT latitude,longitude FROM business where idbusiness=$1"
 	error_shown := db.QueryRow(context.Background(), q, idbusiness).Scan(&latitude, &longitude)
 
 	if error_shown != nil {

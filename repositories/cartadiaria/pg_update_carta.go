c6436249107b1689956ff147fa03623ce4c2b80f
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-04-17 10:29:44 -0500

    v1.46

--- repositories/cartadiaria/pg_update_carta.go
+++ repositories/cartadiaria/pg_update_carta.go
@@ -16,9 +16,28 @@ func Pg_Update_Available_Visible(available bool, visible bool, idcarta int, idbu
 
 	db_external := models.Conectar_Pg_DB_External()
 
-	q := "UPDATE Carta set availableorders=$1,visible=$2,updateddate=$3 WHERE idcarta=$4 AND idbusiness=$5"
-	if _, err_update := db_external.Exec(ctx, q, available, visible, time.Now(), idcarta, idbusiness); err_update != nil {
+	//BEGIN
+	tx, error_tx := db_external.Begin(ctx)
+	if error_tx != nil {
+		tx.Rollback(ctx)
+		return error_tx
+	}
+
+	q_carta := "UPDATE Carta set availableorders=$1,visible=$2,updateddate=$3 WHERE idcarta=$4 AND idbusiness=$5"
+	if _, err_update := tx.Exec(ctx, q_carta, available, visible, time.Now(), idcarta, idbusiness); err_update != nil {
 		return err_update
+	}
+
+	q := "UPDATE Element set availableorders=$1 WHERE idcarta=$2 AND idbusiness=$3"
+	if _, err_update := tx.Exec(ctx, q, available, idcarta, idbusiness); err_update != nil {
+		return err_update
+	}
+
+	//TERMINAMOS LA TRANSACCION
+	err_commit := tx.Commit(ctx)
+	if err_commit != nil {
+		tx.Rollback(ctx)
+		return err_commit
 	}
 
 	return nil

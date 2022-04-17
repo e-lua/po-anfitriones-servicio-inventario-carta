0f1215daa54aa2c8ef8dbd27c0e729e412b08846
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-04-16 20:21:27 -0500

    v1.41

--- repositories/inventario_insumo/mo_search_name.go
+++ repositories/inventario_insumo/mo_search_name.go
@@ -6,6 +6,7 @@ import (
 
 	models "github.com/Aphofisis/po-anfitrion-servicio-inventario-carta/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Mo_Search_Name(idbusiness int, name string) ([]*models.Mo_Insumo_Response, error) {
@@ -17,20 +18,21 @@ func Mo_Search_Name(idbusiness int, name string) ([]*models.Mo_Insumo_Response, 
 
 	var resultado []*models.Mo_Insumo_Response
 
-	pipeline := []bson.M{
-		{"$match": bson.M{"idbusiness": idbusiness}},
-		{"$match": bson.M{"isdeleted": false}},
-		{"$match": bson.M{
-			"name":           "/" + name + "/",
-			"$caseSensitive": true,
+	condicion := bson.M{
+		"idbusiness": idbusiness,
+		"isdeleted":  false,
+		"name": bson.M{
+			"$regex": "/$" + name + "$/",
 		},
-		},
-		{"$sort": bson.M{"tweet.fecha": -1}},
 	}
+
+	opciones := options.Find()
+	/*Indicar como ira ordenado*/
+	opciones.SetSort(bson.D{{Key: "name", Value: 1}})
 
 	/*Cursor es como una tabla de base de datos donde se van a grabar los resultados
 	y podre ir recorriendo 1 a la vez*/
-	cursor, err := col.Aggregate(ctx, pipeline)
+	cursor, err := col.Find(ctx, condicion, opciones)
 	if err != nil {
 		return resultado, err
 	}

cb24febee31c73141932b12139722de9603f58f7
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-04-18 02:23:30 -0500

    v1.52

--- repositories/carta_category/pg_update_sendtodelete.go
+++ repositories/carta_category/pg_update_sendtodelete.go
@@ -17,8 +17,8 @@ func Pg_Update_SendToDelete(idcategory int, timezone int, idbusiness int) error 
 	db := models.Conectar_Pg_DB()
 
 	//Actualizamos la foto de la categoría
-	q := "UPDATE Category SET isexported=false,issendtodelete=true,sendtodelete=$1,deleteddate=$2 WHERE idcategory=$3 AND idbusiness=$4 AND (SELECT COUNT(e.idelement) FROM Category c LEFT OUTER JOIN Element e on c.idcategory=e.idcategory WHERE c.idcategory=$3 AND c.idbusiness=$4 AND e.isdeleted=false GROUP BY c.idcategory)=0"
-	if _, err_update := db.Exec(ctx, q, time.Now().Add(time.Hour*time.Duration(timezone)), time.Now().AddDate(0, 0, 7), idcategory, idbusiness); err_update != nil {
+	q := "UPDATE Category SET isexported=false,issendtodelete=true,sendtodelete=$1,deleteddate=$2 WHERE idcategory=$3 AND idbusiness=$4 AND (SELECT COUNT(e.idelement) FROM Category c LEFT OUTER JOIN Element e on c.idcategory=e.idcategory WHERE c.idcategory=$5 AND c.idbusiness=$6 AND e.isdeleted=false GROUP BY c.idcategory)=0"
+	if _, err_update := db.Exec(ctx, q, time.Now().Add(time.Hour*time.Duration(timezone)), time.Now().AddDate(0, 0, 7), idcategory, idbusiness, idcategory, idbusiness); err_update != nil {
 		return err_update
 	}
 

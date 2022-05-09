990f2666f46c8a54fb536f38d4b1cbe2444a3c9f
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-05-09 01:51:59 -0500

    v1.85

--- services/exportfile/exportfile_router.go
+++ services/exportfile/exportfile_router.go
@@ -43,7 +43,7 @@ func (efr *exportfileRouter_pg) ExportFile_Insumo(c echo.Context) error {
 
 	//Obtenemos los datos del auth
 	respuesta, _ := http.Get("http://a-registro-authenticacion.restoner-api.fun:5000/v1/worker/email")
-	respuesta.Header.Add("Authorization", c.Request().Header.Get("Authorization"))
+	respuesta.Request.Header.Add("Authorization", c.Request().Header.Get("Authorization"))
 	var get_respuesta Response
 	error_decode_respuesta := json.NewDecoder(respuesta.Body).Decode(&get_respuesta)
 	if error_decode_respuesta != nil {

1ac7deeb6e5522bf0f243bb1f7ae96c6e2e0b3f6
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-05-29 00:41:14 -0500

    v1.90

--- repositories/carta_element/pg_update_data.go
+++ repositories/carta_element/pg_update_data.go
@@ -9,10 +9,13 @@ import (
 
 func Pg_Update_Data(element models.Pg_Element, idbusiness int) error {
 
+	//Validar si hay datos de insumos
+	counter_check := 0
+
 	var quantity_insumos int
 	var costo_medio_insumos float64
 
-	if len(element.Insumos) > 0 {
+	if len(element.Insumos) > 0 && element.IsAutomaticCost {
 
 		for _, insumo := range element.Insumos {
 			var quantity_stock int
@@ -23,18 +26,30 @@ func Pg_Update_Data(element models.Pg_Element, idbusiness int) error {
 			}
 			quantity_insumos += 1
 			costo_medio_insumos += ((costo / float64(quantity_stock)) * float64(insumo.Quantity))
-		}
-		element.Costo = (costo_medio_insumos / float64(quantity_insumos))
 
+			//Validar si hay insumos
+			counter_check = counter_check + 1
+		}
+
+		if counter_check != 0 {
+			element.Costo = (costo_medio_insumos / float64(quantity_insumos))
+		} else {
+			element.Costo = 0
+		}
 	}
+
+	if len(element.Insumos) == 0 && element.IsAutomaticCost {
+		element.Costo = 0
+	}
+
 	//Tiempo limite al contexto
 	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
 	//defer cancelara el contexto
 	defer cancel()
 
 	db := models.Conectar_Pg_DB()
-	q := "UPDATE Element SET isexported=false,price=$1,description=$2,typemoney=$3, updateddate=$4,idcategory=$5,insumos=$6,costo=$7 FROM Category WHERE idelement=$8 AND Category.idbusiness=$9"
-	if _, err_update := db.Exec(ctx, q, element.Price, element.Description, element.TypeMoney, time.Now(), element.IDCategory, element.Insumos, element.Costo, element.IDElement, idbusiness); err_update != nil {
+	q := "UPDATE Element SET isexported=false,price=$1,description=$2,typemoney=$3, updateddate=$4,idcategory=$5,insumos=$6,costo=$7,isautomaticcost=$8 FROM Category WHERE idelement=$8 AND Category.idbusiness=$9"
+	if _, err_update := db.Exec(ctx, q, element.Price, element.Description, element.TypeMoney, time.Now(), element.IDCategory, element.Insumos, element.Costo, element.IDElement, idbusiness, element.IsAutomaticCost); err_update != nil {
 		return err_update
 	}
 

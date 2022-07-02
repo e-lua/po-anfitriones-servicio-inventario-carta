61c74d1322a26ae98e176602c5e8ee95a1a07c57
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-07-02 18:06:03 -0500

    v1.106

--- services/pre_charged/pre_charged_router.go
+++ services/pre_charged/pre_charged_router.go
@@ -32,7 +32,7 @@ func (ir *importsRouter_pg) AddPreCharged(c echo.Context) error {
 
 func (ir *importsRouter_pg) AddPreCharged_Multiple(c echo.Context) error {
 
-	var pre_charged_multiple Request_Precarga
+	var pre_charged_multiple []models.Mo_Precharged_Element
 
 	err := c.Bind(&pre_charged_multiple)
 	if err != nil {
@@ -41,7 +41,7 @@ func (ir *importsRouter_pg) AddPreCharged_Multiple(c echo.Context) error {
 	}
 
 	//Enviamos los datos al servicio
-	status, boolerror, dataerror, data := AddPreCharged_Multiple_Service(pre_charged_multiple.AllPrecharged)
+	status, boolerror, dataerror, data := AddPreCharged_Multiple_Service(pre_charged_multiple)
 	results := Response{Error: boolerror, DataError: dataerror, Data: data}
 	return c.JSON(status, results)
 

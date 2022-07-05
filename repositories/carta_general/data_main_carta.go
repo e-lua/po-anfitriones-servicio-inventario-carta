1a257737657e6fc4a7d01aa89f5869c6ff8506a8
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-07-05 13:11:54 -0500

    v1.109

--- repositories/carta_general/data_main_carta.go
+++ repositories/carta_general/data_main_carta.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	models "github.com/Aphofisis/po-anfitrion-servicio-inventario-carta/models"
@@ -16,7 +17,7 @@ func Pg_Find_Main_Data(idbsuiness int) (models.Pg_Category_Element_ScheduleRange
 
 	var oCES models.Pg_Category_Element_ScheduleRange
 
-	db := models.Conectar_Pg_DB(1)
+	db := models.Conectar_Pg_DB(rand.Intn(5))
 
 	q := "SELECT (SELECT count(*) FROM category WHERE idbusiness=$1 AND available=true), (SELECT count(*) FROM element e JOIN category c on e.idcategory=c.idcategory WHERE c.idbusiness=$2 AND e.available=true),(SELECT count(*) FROM ScheduleRange WHERE idbusiness=$3 AND available=true)"
 	error_showname := db.QueryRow(ctx, q, idbsuiness, idbsuiness, idbsuiness).Scan(&oCES.Category, &oCES.Element, &oCES.Schedule)

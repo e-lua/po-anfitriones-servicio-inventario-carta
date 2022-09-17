ecac0f699928a7c9898ad86db1f523117ef4b3b7
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-09-17 15:45:59 -0500

    v1.155

--- repositories/carta_category/pg_find_papelera.go
+++ repositories/carta_category/pg_find_papelera.go
@@ -15,7 +15,7 @@ func Pg_Find_Papelera(idbusiness int) ([]models.Pg_Category_Response, error) {
 	defer cancel()
 
 	db := models.Conectar_Pg_DB()
-	q := "SELECT c.idcategory, COUNT(e.idelement) as elements ,c.name,c.urlphoto,c.available,c.typefood,c.sendtodelete FROM Category c LEFT OUTER JOIN Element e on c.idcategory=e.idcategory WHERE c.idbusiness=$1 AND c.isdeleted=false AND c.issendtodelete=true GROUP BY c.idcategory ORDER BY c.sendtodelete DESC"
+	q := "SELECT c.idcategory, COUNT(e.idelement) as elements ,c.name,c.urlphoto,c.available,c.typefood,c.sendtodelete,c.deleteddate FROM Category c LEFT OUTER JOIN Element e on c.idcategory=e.idcategory WHERE c.idbusiness=$1 AND c.isdeleted=false AND c.issendtodelete=true GROUP BY c.idcategory ORDER BY c.sendtodelete DESC"
 	rows, error_shown := db.Query(ctx, q, idbusiness)
 
 	//Instanciamos una variable del modelo Pg_TypeFoodXBusiness
@@ -29,7 +29,7 @@ func Pg_Find_Papelera(idbusiness int) ([]models.Pg_Category_Response, error) {
 	//Scaneamos l resultado y lo asignamos a la variable instanciada
 	for rows.Next() {
 		oCategory := models.Pg_Category_Response{}
-		rows.Scan(&oCategory.IDCategory, &oCategory.Elements, &oCategory.Name, &oCategory.UrlPhoto, &oCategory.Available, &oCategory.TypeFood, &oCategory.SendToDelete)
+		rows.Scan(&oCategory.IDCategory, &oCategory.Elements, &oCategory.Name, &oCategory.UrlPhoto, &oCategory.Available, &oCategory.TypeFood, &oCategory.SendToDelete, &oCategory.DeletedDate)
 		oListCategory = append(oListCategory, oCategory)
 	}
 
--- repositories/carta_element/pg_find_papelera.go
+++ repositories/carta_element/pg_find_papelera.go
@@ -16,7 +16,7 @@ func Pg_Find_Papelera(idbusiness int) ([]models.Pg_Element_Tofind, error) {
 
 	db := models.Conectar_Pg_DB()
 
-	q := "SELECT c.typefood,c.idcategory,COALESCE(c.urlphoto,'https://restoner-public-space.sfo3.cdn.digitaloceanspaces.com/restoner-general/default-image/default-img.png'),c.name,e.idelement,e.name,e.description,e.typemoney,e.price,COALESCE(e.urlphoto,'noimage'),e.available,e.sendtodelete,e.insumos,e.costo FROM element e JOIN category c on e.idcategory=c.idcategory WHERE c.idbusiness=$1 AND e.isdeleted=false AND e.issendtodelete=true ORDER BY e.sendtodelete DESC"
+	q := "SELECT c.typefood,c.idcategory,COALESCE(c.urlphoto,'https://restoner-public-space.sfo3.cdn.digitaloceanspaces.com/restoner-general/default-image/default-img.png'),c.name,e.idelement,e.name,e.description,e.typemoney,e.price,COALESCE(e.urlphoto,'noimage'),e.available,e.sendtodelete,e.insumos,e.costo,e.deleteddate FROM element e JOIN category c on e.idcategory=c.idcategory WHERE c.idbusiness=$1 AND e.isdeleted=false AND e.issendtodelete=true ORDER BY e.sendtodelete DESC"
 	rows, error_shown := db.Query(ctx, q, idbusiness)
 
 	//Instanciamos una variable del modelo Pg_TypeFoodXBusiness
@@ -30,7 +30,7 @@ func Pg_Find_Papelera(idbusiness int) ([]models.Pg_Element_Tofind, error) {
 	//Scaneamos l resultado y lo asignamos a la variable instanciada
 	for rows.Next() {
 		oElement := models.Pg_Element_Tofind{}
-		rows.Scan(&oElement.Typefood, &oElement.IDCategory, &oElement.URLPhotoCategory, &oElement.NameCategory, &oElement.IDElement, &oElement.Name, &oElement.Description, &oElement.TypeMoney, &oElement.Price, &oElement.UrlPhoto, &oElement.Available, &oElement.SendToDelete, &oElement.Insumos, &oElement.Costo)
+		rows.Scan(&oElement.Typefood, &oElement.IDCategory, &oElement.URLPhotoCategory, &oElement.NameCategory, &oElement.IDElement, &oElement.Name, &oElement.Description, &oElement.TypeMoney, &oElement.Price, &oElement.UrlPhoto, &oElement.Available, &oElement.SendToDelete, &oElement.Insumos, &oElement.Costo, &oElement.DeletedDate)
 		oListElement = append(oListElement, oElement)
 	}
 
